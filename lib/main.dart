ad85854220367ca61bb862a9be4f0c200017c496
Arpit Patel <arpitpatel009@gmail.com>
2020-03-25 19:50:29 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 70.0,
+                backgroundColor: Colors.teal[200],
+                backgroundImage: AssetImage("images/arpit.jpg"),
+                foregroundColor: Color.fromARGB(70, 10, 20, 30),
+              ),
+              Text(
+                "Arpit Patel",
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade50,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+1 (639) 998-2747',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'arpitpatel009@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//Row(
+//                  children: <Widget>[
+//                    Icon(
+//                      Icons.email,
+//                      color: Colors.teal,
+//                    ),
+//                    SizedBox(
+//                      width: 10,
+//                    ),
+//                    Text(
+//                      'arpitpatel009@gmail.com',
+//                      style: TextStyle(
+//                          fontSize: 20,
+//                          fontWeight: FontWeight.bold,
+//                          fontFamily: 'Source Sans Pro',
+//                          color: Colors.teal.shade900),
+//                    ),
+//                  ],
+//                ),
